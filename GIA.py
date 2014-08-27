9e4f28041252f7c0c258c640068762eec641fc24
markm541374 <mark.mcleod.17@googlemail.com>
2014-08-27 22:37:55 +0100

    more writing

--- GIA.py
+++ GIA.py
@@ -25,7 +25,7 @@ var = sum([((i-mean)**2)/float(N) for i in data])
 scale=var/mean
 shape=mean/scale
 
-hp,bins,patches=plt.hist(data,bins=30,normed=1,facecolor='green',alpha=0.5)
+hp,bins,patches=plt.hist(data,bins=30,normed=1,facecolor='green',alpha=0.4)
 cbins=[i-0.5*(bins[1]-bins[0]) for i in bins[1:]]
 
 best=sp.Inf
@@ -47,9 +47,9 @@ plt.plot(sp.linspace(0,max(data),100),[jmax[0].pdf(i) for i in sp.linspace(0,max
 print "loc = "+str(jmax[3])
 print "shape = "+str(jmax[1])
 print "scale = "+str(jmax[2])
-
-
-
+plt.xlabel("Internal Area (m^2)")
+plt.ylabel("p(A)")
+plt.savefig("f3.eps")
 plt.show()
 
 
